6ca20d61a16bb0f8c35b2a1a53f9d1359be7936a
Igor <redway@mail.ru>
2020-02-18 22:35:49 +0600

    HW20

---
+++ hotel/hotelapp/management/commands/fill_db.py
@@ -0,0 +1,8 @@
+from django.core.management.base import BaseCommand, CommandError
+from hotelapp.models import Room, Client, BookingOrder, HistoryPrice
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        print('Работаем!')
