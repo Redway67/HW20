ac34ed10bb2647af5ff80e6123ce9fad58a8e034
Igor <redway@mail.ru>
2020-04-11 22:02:09 +0600

    HW25-Tools

--- hotel/filterapp/templatetags/filter_extensions.py
+++ hotel/filterapp/templatetags/filter_extensions.py
@@ -4,8 +4,9 @@ from datetime import date
 register = template.Library()
 
 
+# Возвращает количество дней до события, если событие в прошлом, то 0
 def rest_day(book_date):
-    return (book_date-date.today()).days if book_date > date.today() else 0
+    return (book_date - date.today()).days if book_date > date.today() else 0
 
 
 register.filter('rest_day', rest_day)
