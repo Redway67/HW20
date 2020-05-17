e2adff21dc80cc264b2ca9d576651caed19a6747
Igor <redway@mail.ru>
2020-05-17 20:21:49 +0600

    HW31-api

--- hotel/hotelapp/api_views.py
+++ hotel/hotelapp/api_views.py
@@ -1,13 +1,19 @@
 from .models import Client, Room
 from .serializers import ClientSerializer, RoomSerializer
 from rest_framework import viewsets
+from .permissions import ReadOnly, IsSuperUser
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
+from rest_framework.authentication import BasicAuthentication, SessionAuthentication, TokenAuthentication
 
 
 class ClientViewSet(viewsets.ModelViewSet):
+    # authentication_classes = [SessionAuthentication, BasicAuthentication, TokenAuthentication]
+    permission_classes = [IsAuthenticated]
     queryset = Client.objects.all()
     serializer_class = ClientSerializer
 
 
 class RoomViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsSuperUser]
     queryset = Room.objects.all()
     serializer_class = RoomSerializer
--- hotel/hotelapp/test_views.py
+++ hotel/hotelapp/test_views.py
@@ -44,3 +44,30 @@ class ViewsTest(TestCase):
         response = self.client.get('/administration/?page=10')
         self.assertEqual(response.status_code, 404)
 
+    def test_api(self):
+        admin_user = HotelUser.objects.create_user(username='hotelier', email='redway@mail.ru',
+                                                   password='anapa2020', is_superuser=True)
+        self.assertTrue(admin_user.is_superuser)
+        response = self.client.get('/api/v0/clients/')
+        self.assertNotEqual(response.status_code, 200)  # надо зайти
+
+        self.client.login(username='hotelier', password='anapa2020')
+
+        response = self.client.get('/api/v0/clients/')  # зашли, теперь можно
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.get('/api/v0/rooms/')  # только суперюзер
+        self.assertEqual(response.status_code, 200)
+
+        self.client.logout()
+
+        # пробуем не суперюзера
+        h_user = HotelUser.objects.create_user(username='ivanov', email='ivanov@m.ru', password='yj@y2mzaHb@6W9C')
+        self.assertFalse(h_user.is_superuser)
+        self.client.login(username='ivanov', password='yj@y2mzaHb@6W9C')
+
+        response = self.client.get('/api/v0/rooms/')
+        self.assertNotEqual(response.status_code, 200)  # только суперюзер
+
+        response = self.client.get('/api/v0/clients/')  # а здесь любой
+        self.assertEqual(response.status_code, 200)
